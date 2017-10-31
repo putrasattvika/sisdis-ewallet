25e434f968e1c030dc636d8d05ac747071f4d73c
Putra Sattvika <sattvikaputra@gmail.com>
2017-10-31 15:24:30 +0700

    add custom validator to handle HTTP reqs w/o Content-Type

--- src/app.py
+++ src/app.py
@@ -2,12 +2,15 @@
 import logging
 import argparse
 import connexion
+import ewallet
 
 from ewallet import *
 
 __DEFAULT_ID = '1406527532'
 __DEFAULT_HOST = '0.0.0.0'
 __DEFAULT_PORT = 10034
+
+logger = logging.getLogger('ewallet.app')
 
 def get_args():
 	parser = argparse.ArgumentParser(add_help=False)
@@ -26,10 +29,15 @@ def main():
 
 	ewallet_init('ewallet.db', str(args.id), debug=args.debug)
 
-	app = connexion.App(__name__)
+	validator_map = {
+		'body': ewallet.helper.validation.FormJsonRequestBodyValidator,
+		'parameter': connexion.decorators.validation.ParameterValidator
+	}
+
+	app = connexion.App(__name__, validator_map=validator_map)
 	app.add_api('ewallet.yaml', strict_validation=True)
 
-	print '[+] sttv_ewallet running on {}:{} with debug={} and id={}'.format(args.host, args.port, args.debug, args.id)
+	logger.info('[+] sttv_ewallet running on {}:{} with debug={} and id={}'.format(args.host, args.port, args.debug, args.id))
 	app.run(host=args.host, port=args.port, threaded=True)
 
 if __name__ == '__main__':
--- src/ewallet/helper/__init__.py
+++ src/ewallet/helper/__init__.py
@@ -1,2 +1,4 @@
+import validation
+
 from quorum import quorum
 from quorum import inquire as quorum_inquire
