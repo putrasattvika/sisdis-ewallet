31da929547a045920220c48034a3c3fba0932564
Putra Sattvika <sattvikaputra@gmail.com>
2017-12-03 00:20:56 +0700

    migrate get-saldo logic

--- src/ewallet/msgqueue/get_saldo/get_saldo_consumer.py
+++ src/ewallet/msgqueue/get_saldo/get_saldo_consumer.py
@@ -2,6 +2,7 @@ import json
 import logging
 
 from ewallet.helper import db
+from ewallet.helper import codes
 from ewallet.helper import settings
 from ewallet.helper import definition
 from ewallet.msgqueue.base_consumer import BaseConsumer
@@ -22,12 +23,22 @@ class GetSaldoConsumer(BaseConsumer):
 			exchange = settings.mq_get_saldo['exchange']
 			routing_key = 'RESP_{}'.format(j['sender_id'])
 
-			balance = -1
-			user = db.get_user(j['user_id'])
-			if user:
-				balance = user['balance']
+			try:
+				user = db.get_user(j['user_id'])
 
-			response = json.dumps(definition.balance_inquiry_response(balance))
+				status_code = None
+				if not user:
+					status_code = codes.USER_DOES_NOT_EXISTS_ERROR
+				else:
+					balance = user['balance']
+			except DBError as e:
+				status_code = codes.DATABASE_ERROR
+				logger.info('[-] Database error: %s', e.message)
+			except Exception as e:
+				status_code = codes.UNKNOWN_ERROR
+				logger.info('[-] Unknown error: %s', e.message)
+
+			response = json.dumps(definition.balance_inquiry_response(balance, status_code=status_code))
 
 			reply_queue = ch.queue_declare()
 			ch.queue_bind(
